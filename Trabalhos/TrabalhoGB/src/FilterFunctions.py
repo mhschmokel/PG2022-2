b2303f7745b7c049a176aeff4110135239b92f60
Matheus Schmokel <matheus.schmokel@gmail.com>
2022-11-20 15:31:59 -0300

    Finalizada etapa dos filtros

--- Trabalhos/TrabalhoGB/src/FilterFunctions.py
+++ Trabalhos/TrabalhoGB/src/FilterFunctions.py
@@ -1,6 +1,9 @@
 import cv2 as cv
 from Filter import *
 
+def noFilterFunction(img):
+  return img
+
 def blurFunction(img):
   return cv.GaussianBlur(img, (13, 13), 5, 0)
 
@@ -17,13 +20,28 @@ def pencilSketchFunction(img):
   sk_gray, sk_color = cv.pencilSketch(img, sigma_s=60, sigma_r=0.07, shade_factor=0.1)
   return sk_color
 
+def hdrFunction(img):
+  return cv.detailEnhance(img, sigma_s=12, sigma_r=0.15)
+
+def equalizeFunction(img):
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    return cv.equalizeHist(img)
+
+def dilateFunction(img):
+  kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
+  return cv.dilate(img, kernel)
+
 def handleFilter(*args):
     filters[args[1]].isActive = not filters[args[1]].isActive
 
 filters = {
+            'noFilter':Filter(noFilterFunction),
             'blur': Filter(blurFunction),
             'gray': Filter(grayFunction),
             'invert': Filter(invertFunction),
             'canny': Filter(cannyFunction),
             'pencilSketch': Filter(pencilSketchFunction),
+            'hdr': Filter(hdrFunction),
+            'equalize': Filter(equalizeFunction),
+            'dilate': Filter(dilateFunction)
         }
--- Trabalhos/TrabalhoGB/src/filterController.py
+++ Trabalhos/TrabalhoGB/src/filterController.py
@@ -4,8 +4,12 @@ from FilterFunctions import *
 class filterController():
 
     def initButtons(*args, cv):
-        cv.createButton('Blur', handleFilter, 'blur', cv.QT_CHECKBOX, False)
-        cv.createButton('Gray', handleFilter, 'gray', cv.QT_CHECKBOX, False)
-        cv.createButton('Invert', handleFilter, 'invert', cv.QT_CHECKBOX, False)
-        cv.createButton('Canny', handleFilter, 'canny', cv.QT_CHECKBOX, False)
-        cv.createButton('Pencil Sketch', handleFilter, 'pencilSketch', cv.QT_CHECKBOX, False)
+        cv.createButton('No filter', handleFilter, 'noFilter', cv.QT_RADIOBOX, True)
+        cv.createButton('Blur', handleFilter, 'blur', cv.QT_RADIOBOX, False)
+        cv.createButton('Gray', handleFilter, 'gray', cv.QT_RADIOBOX, False)
+        cv.createButton('Invert', handleFilter, 'invert', cv.QT_RADIOBOX, False)
+        cv.createButton('Canny', handleFilter, 'canny', cv.QT_RADIOBOX, False)
+        cv.createButton('Pencial Sketch', handleFilter, 'pencilSketch', cv.QT_RADIOBOX, False)
+        cv.createButton('HDR', handleFilter, 'hdr', cv.QT_RADIOBOX, False)
+        cv.createButton('Equalize', handleFilter, 'equalize', cv.QT_RADIOBOX, False)
+        cv.createButton('Dilate', handleFilter, 'dilate', cv.QT_RADIOBOX, False)
